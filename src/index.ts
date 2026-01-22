0f3f5f6e825f1bff22baf745ae032114181f1e4c
MoonManBoolin <jellyeater343@gmail.com>
2026-01-22 11:58:11 -0800

    added commands registry and login command

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { setUser, readConfig } from "./config";
+import { CommandsRegistry, CommandHandler, registerCommand, runCommand, handlerLogin } from "./commands_registry";
+import { argv } from 'node:process';
 
 function main() {
-  setUser("moonman")
-  const result = readConfig()
-  console.log(result)
+  const registry: CommandsRegistry = {}
+  registerCommand(registry, "login", handlerLogin)
+  const cmds = argv.slice(2)
+  if (!cmds.length) {
+    console.error('Not enough arguments')
+    process.exit(1)
+  }
+  const cmdName = cmds[0]
+  const args = cmds.slice(1)
+  console.log(`Command: ${cmdName}\nArgs: ${args}`)
+  runCommand(registry, cmdName, ...args)
 }
 
 main();
